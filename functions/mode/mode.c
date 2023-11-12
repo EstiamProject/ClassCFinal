cdadd61d5facac6a9b2c0ff0abaf2c2d4b152723
Tuke <75539193+TukeCS@users.noreply.github.com>
2023-11-12 09:28:03 +0100

    test push

--- functions/mode/mode.c
+++ functions/mode/mode.c
@@ -6,13 +6,13 @@
 #include "../../misc/utils.h"
 
 void mode(char * input){
-    
+
     if (!strcmp(input, "calc")){
         printf("mode calc selected\n");
     } else if (!strcmp(input, "moy")) {
         printf("mode moy selected\n");
     } else if (!strcmp(input, "todo")) {
-        printf("mode todo selected\n");
+        printf("mode todo selected\n ");
     } else if (!strcmp(input, "exit")) {
         printf("quitting modes\n");
     } else {
