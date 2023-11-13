db0d32f6bfd1aa9f652bb85711a25dfd7d9cb7d9
Tuke <75539193+TukeCS@users.noreply.github.com>
2023-11-13 13:44:16 +0100

    calc finished only todo left
    yipee

--- functions/calc/calc.c
+++ functions/calc/calc.c
@@ -3,9 +3,30 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "../../misc/utils.h"
 
 void calc(char * input){
 
-    printf("%s", input);
+    int result = 0, temp = 0;
+    char* token = strtok(input, " ");
+
+    while (token != NULL) {
+        if (!strcmp(token, "+")) {
+            token = strtok(NULL, " ");
+            result += atoi(token);
+        }
+        else if (!strcmp(token, "-")) {
+            token = strtok(NULL, " ");
+            result -= atoi(token);
+        }
+        else{
+            result += atoi(token);
+        }
+
+        token = strtok(NULL, " ");
+    }
+
+    printf("%d\n", result);
 }
